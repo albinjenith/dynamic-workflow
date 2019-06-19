ebd55133a37fa2517f7cfd7ff4b852ce84186775
Albin Jenith <albinjenith@gmail.com>
2019-06-19 22:11:31 +0530

    Initial Setup

---
+++ src/main/java/com/bpm/dynamicworkflow/DynamicWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.bpm.dynamicworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpm/dynamicworkflow/DynamicWorkflowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bpm.dynamicworkflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamicWorkflowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
