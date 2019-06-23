5838a37d8026f15be4a5735cb678fbd02c992e24
Albin Jenith <albinjenith@gmail.com>
2019-06-23 14:02:38 +0530

    Activiti initialized

--- src/test/java/com/bpm/dynamicworkflow/DynamicWorkflowApplicationTests.java
+++ src/test/java/com/bpm/dynamicworkflow/DynamicWorkflowApplicationTests.java
@@ -1,16 +1,15 @@
-package com.bpm.dynamicworkflow;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DynamicWorkflowApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+/*
+ * package com.bpm.dynamicworkflow;
+ * 
+ * import org.junit.Test; import org.junit.runner.RunWith; import
+ * org.springframework.boot.test.context.SpringBootTest; import
+ * org.springframework.test.context.junit4.SpringRunner;
+ * 
+ * @RunWith(SpringRunner.class)
+ * 
+ * @SpringBootTest public class DynamicWorkflowApplicationTests {
+ * 
+ * @Test public void contextLoads() { }
+ * 
+ * }
+ */
